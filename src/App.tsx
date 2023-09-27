d2e13553bc3a467fb76bbbec247e37a168b449e1
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-27 14:40:04 +0000

    #Basic Animations

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 30px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+        transition={{ type: "spring", delay: 0.5 }}
+      />
+    </Wrapper>
+  );
+}
+
+export default App;
