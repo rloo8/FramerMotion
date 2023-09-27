de1ac32eedd90052a5be4e2708e2a919859da0db
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-27 14:42:39 +0000

    #2 variants

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVar = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-        transition={{ type: "spring", delay: 0.5 }}
-      />
+      <Box variants={myVar} initial="start" animate="end" />
     </Wrapper>
   );
 }
