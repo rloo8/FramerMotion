3b9af090b01f225fa4aeadd79a87ca13f3e78628
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-28 23:27:10 +0900

    #8_2 Layout animation 2

--- App.tsx
+++ App.tsx
@@ -11,31 +11,55 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 const Box = styled(motion.div)`
-  width: 400px;
   height: 400px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  background-color: #00a5ff;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 15px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false);
-  const toggleClicked = () => setIsClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
 
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>{!isClicked ? <Circle layoutId="circle" /> : null}</Box>
-      <Box>{isClicked ? <Circle layoutId="circle" /> : null}</Box>
+    <Wrapper>
+      <Grid>
+        {[1, 2, 3, 4].map((n) => (
+          <Box onClick={() => setId(n + "")} key={n} layoutId={n + ""} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0,0,0,0" }}
+            animate={{ backgroundColor: "rgba(0,0,0,0.6" }}
+            exit={{ backgroundColor: "rgba(0,0,0,0" }}
+          >
+            <Box layoutId={id} style={{ width: 600, height: 400 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
