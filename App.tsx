4e253f2d16ad4cb34c534b4e08516965f512ab6b
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-28 22:56:01 +0900

    #8 Layout animation

--- App.tsx
+++ App.tsx
@@ -11,75 +11,31 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 400px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const boxVarient = {
-  entry: (isBack: boolean) => {
-    return {
-      x: isBack ? -300 : 300,
-      opacity: 0,
-      scale: 0,
-    };
-  },
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: (isBack: boolean) => {
-    return {
-      x: isBack ? 300 : -300,
-      opacity: 0,
-      scale: 0,
-      transition: {
-        duration: 0.5,
-      },
-    };
-  },
-};
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: #00a5ff;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const prevBtn = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-  const nextBtn = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
+  const [isClicked, setIsClicked] = useState(false);
+  const toggleClicked = () => setIsClicked((prev) => !prev);
 
   return (
-    <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={boxVarient}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-
-      <button onClick={prevBtn}>prev</button>
-      <button onClick={nextBtn}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>{!isClicked ? <Circle layoutId="circle" /> : null}</Box>
+      <Box>{isClicked ? <Circle layoutId="circle" /> : null}</Box>
     </Wrapper>
   );
 }
